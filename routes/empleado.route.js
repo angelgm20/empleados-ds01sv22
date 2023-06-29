4b88d927a06b2547123e2827c5820a82970ec4c4
angelica <angelicamm.ti20@utsjr.edu.mx>
2023-06-28 21:50:08 -0600

    Versión 1 de API

---
+++ routes/empleado.route.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const empleadoRuta = express.Router()
+
+// declaramos un objeto de nuestro modelo
+let Empleado = require('../models/Empleado')
+
+//Método para agregar un nuevo empleado
+
+empleadoRuta.route('/create').post((req,res)=>{
+    Empleado.create(req.body)
+    .then((data) => {
+        console.log('Se insertó un nuevo documento')
+        res.send(data)
+
+    })
+    .catch((err) =>{
+        console.error(err)
+    })
+})
+
+//obtenemos todos los empleados
+
+empleadoRuta.route('/empleados').get((req,res) => {
+    Empleado.find()
+    .then((data) => {
+        res.send(data)
+    })
+    .catch((err) => {
+        console.log.error(err)
+    })
+})
+
+//Obtener un solo empleado por su Id
+
+empleadoRuta.route('/empleado/:id').get((req,res) =>{
+    Empleado.findById(req.params.id)
+    .then((data) =>{
+        res.send(data)
+    })
+    .catch((err) =>{
+        console.error(err)
+    })
+})
+
+//Actualizar un empleado
+empleadoRuta.route('/update/:id').put((req,res) =>{
+    Empleado.findByIdAndUpdate(req.params.id,{
+        $set: req.body
+    })
+    .then+((data) =>{
+        res.send(data)
+    })
+    .catch((err) =>{
+        console.error(err)
+    })
+})
+
+//Eliminar un empleado
+empleadoRuta.route('/delete/:id').delete((req,res) =>{
+    Empleado.findByIdAndRemove(req.params.id)
+    .then((data) => {
+        res.send(data)
+    })
+    .catch((err) =>{
+        console.error(err)
+    })
+})
+
+module.exports = empleadoRuta;
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser =require('body-parser')
+const path = require('path')
+
+//conexión con la BD
+mongoose
+    //CONEXION LOCAL .connect('mongodb://127.0.0.1:27017/empleados02sv22')
+    .connect('mongodb+srv://angelicammti20:1234@cluster1.eulyuhd.mongodb.net/empleadosds02sv22?retryWrites=true&w=majority')
+     .then((x) =>{
+        console.log(`Conectado exitosamente a la base de datos: "${x.connections[0].name}"`)
+     })
+     .catch((err) => {
+        console.log('Error al conectarse a Mongo', err.reason) 
+     })
+
+// configuarción del servidor web
+const empleadoRuta = require('./routes/empleado.route')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+)
+
+app.use(cors())
+app.use(express.static(path.join(__dirname,'dist/empleados-mean')))
+app.use('/',express.static(path.join(__dirname,'dist/empleados-mean')))
+app.use('/api',empleadoRuta)
+
+//Habilitar el puerto por el cual se van
+
+const port = process.env.PORT || 4000
+const server = app.listen(port,() =>{
+    console.log('Conectado al puerto '+port)
+})
+
+
+//manejador de error 404
+
+app.use((req,res,next) => {
+    next(createError(404))
+})
+
+//Manejador general de errores
+
+app.use(function(err,req,res,next){
+    console.log(err.message)
+    if(!err.statusCode) err.statusCode = 500
+    res.status(err.statusCode).send(err.message)
+})
